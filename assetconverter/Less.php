08fac61e38025f4a8ed74fc6bc5ba8382048d893
Vitaliy Syrchikov <maddoger@gmail.com>
2013-12-23 18:08:40 +0400

    Less converter.

---
+++ assetconverter/Less.php
@@ -0,0 +1,51 @@
+<?php
+namespace usporting\core\assetconverter;
+use Yii;
+use yii\caching\FileCache;
+
+
+class Less extends Parser
+{
+    /**
+     * @var string the directory or path alias pointing to where less parser is located.
+     */
+    public $lessParserPath = '@app/extensions/assetparser/vendors/lessphp/lessc.inc.php';
+
+    public $auto = false;
+
+
+
+
+    /**
+     * Parse a Less file to CSS
+     */
+    public function parse($src, $dst, $options)
+    {
+        $this->auto = isset($options['auto']) ? $options['auto'] : $this->auto;
+        try {
+            require_once(Yii::getAlias($this->lessParserPath));
+            if ($this->auto) {
+                /* @var FileCache $cacheMgr */
+                $cacheMgr = Yii::createObject('yii\caching\FileCache');
+                $cacheMgr->init();
+                $cacheId = 'less#' . $dst;
+                $cache = $cacheMgr->get($cacheId);
+                if ($cache === false || (@filemtime($dst) < @filemtime($src))) {
+                    $cache = $src;
+                }
+                $less = new \lessc();
+                $newCache = $less->cachedCompile($cache);
+
+                if (!is_array($cache) || ($newCache["updated"] > $cache["updated"])) {
+                    $cacheMgr->set($cacheId, $newCache);
+                    file_put_contents($dst, $newCache['compiled']);
+                }
+            } else {
+                $less = new \lessc();
+                $less->compileFile($src, $dst);
+            }
+        } catch (exception $e) {
+            throw new Exception(__CLASS__ . ': Failed to compile less file : ' . $e->getMessage() . '.');
+        }
+    }
+}
---
+++ assetconverter/Parser.php
@@ -0,0 +1,16 @@
+<?php
+namespace usporting\core\assetconverter;
+use yii\base\Object;
+
+abstract class Parser extends Object
+{
+    /**
+     * Parse a asset file.
+     *
+     * @param string $src source file path
+     * @param string $dst destination file path
+     * @param array $options  parser options
+     * @return mixed
+     */
+    abstract public function parse($src, $dst,  $options);
+}
