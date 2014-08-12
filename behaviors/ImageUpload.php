ca8a35d92d65bcd5bc00a0e4c8bb1cb470d0306f
Vitaliy Syrchikov <maddoger@gmail.com>
2014-08-12 15:16:41 +0400

    Фикс

--- behaviors/ImageUpload.php
+++ behaviors/ImageUpload.php
@@ -23,134 +23,140 @@ use Yii;
  */
 class ImageUpload extends FileUpload
 {
-	/**
-	 * Widget preview height
-	 * @var int
-	 */
-	public $previewHeight;
-	/**
-	 * Widget preview width
-	 * @var int
-	 */
-	public $previewWidth;
+    /**
+     * Widget preview height
+     * @var int
+     */
+    public $previewHeight;
+    /**
+     * Widget preview width
+     * @var int
+     */
+    public $previewWidth;
 
-	/**
-	 * @var array Settings for image auto-generation
-	 * @note
-	 * 'preview' & 'original' versions names are reserved for image preview in widget
-	 * and original image files
-	 * @example
-	 *  array(
-	 *       'small' => array(
-	 *              'resize' => array(200, null),
-	 *       ),
-	 *      'medium' => array(
-	 *              'resize' => array(800, null),
-	 *      )
-	 *  );
-	 */
-	public $versions;
+    /**
+     * @var array Settings for image auto-generation
+     * @note
+     * 'preview' & 'original' versions names are reserved for image preview in widget
+     * and original image files
+     * @example
+     *  array(
+     *       'small' => array(
+     *              'resize' => array(200, null),
+     *       ),
+     *      'medium' => array(
+     *              'resize' => array(800, null),
+     *      )
+     *  );
+     */
+    public $versions;
 
-	/**
-	 * @inheritdoc
-	 */
-	public function attach($owner)
-	{
-		parent::attach($owner);
+    /**
+     * @inheritdoc
+     */
+    public function attach($owner)
+    {
+        parent::attach($owner);
 
-		if (!isset($this->versions['original']))
-			$this->versions['original'] = array();
-		if (!isset($this->versions['preview']) && ($this->previewWidth || $this->previewHeight))
-			$this->versions['preview'] = array('fit' => array($this->previewWidth, $this->previewHeight));
-	}
+        if (!isset($this->versions['original']))
+            $this->versions['original'] = array();
+        if (!isset($this->versions['preview']) && ($this->previewWidth || $this->previewHeight))
+            $this->versions['preview'] = array('fit' => array($this->previewWidth, $this->previewHeight));
+    }
 
-	public function getUrl($version='original')
-	{
-		/*if (!$this->hasVersion($version)) {
-			$this->deleteVersionImages();
-			$this->updateImages();
-		}*/
-		$url = $this->owner->getAttribute($this->attribute);
-		if (empty($url)) return null;
+    public function getUrl($version='original')
+    {
+        /*if (!$this->hasVersion($version)) {
+            $this->deleteVersionImages();
+            $this->updateImages();
+        }*/
+        $url = $this->owner->getAttribute($this->attribute);
+        if (empty($url)) return null;
 
-		if ($version == 'original') {
-			return $url;
-		} else {
-			$info = pathinfo($url);
-			return $info['dirname']. DIRECTORY_SEPARATOR . $version . '_'  . $info['filename']  .'.'. $info['extension'];
-		}
-	}
+        if ($version == 'original') {
+            return $url;
+        } else {
+            $info = pathinfo($url);
+            return $info['dirname']. DIRECTORY_SEPARATOR . $version . '_'  . $info['filename']  .'.'. $info['extension'];
+        }
+    }
 
-	public function getPath($version='original')
-	{
-		$url = $this->owner->getAttribute($this->attribute);
-		if (empty($url)) return null;
+    public function getPath($version='original')
+    {
+        $url = $this->owner->getAttribute($this->attribute);
+        if (empty($url)) return null;
 
-		$filePath = $this->getPathFromUrl($url);
+        $filePath = $this->getPathFromUrl($url);
 
-		if ($version == 'original') {
-			return $filePath;
-		} else {
-			$info = pathinfo($filePath);
-			return $info['dirname']. DIRECTORY_SEPARATOR . $version . '_'  . $info['filename']  .'.'. $info['extension'];
-		}
-	}
+        if ($version == 'original') {
+            return $filePath;
+        } else {
+            $info = pathinfo($filePath);
+            return $info['dirname']. DIRECTORY_SEPARATOR . $version . '_'  . $info['filename']  .'.'. $info['extension'];
+        }
+    }
 
-	public function hasVersion($version = 'original')
-	{
-		$originalImage = $this->getPath($version);
-		return file_exists($originalImage);
-	}
+    public function hasVersion($version = 'original')
+    {
+        $originalImage = $this->getPath($version);
+        return file_exists($originalImage);
+    }
 
-	/**
-	 * Update images
-	 */
-	public function updateImages()
-	{
-		//create image preview for gallery manager
-		$original = $this->getPath();
-		if (!file_exists($original)) return false;
-		foreach ($this->versions as $version => $actions) {
-			/** @var \yii\image\drivers\Image $image */
-			$image = Yii::$app->image->load($original);
+    /**
+     * Update images
+     */
+    public function updateImages()
+    {
+        //create image preview for gallery manager
+        $original = $this->getPath();
+        if (!file_exists($original)) return false;
+        foreach ($this->versions as $version => $actions) {
+            /** @var \yii\image\drivers\Image $image */
+            try {
+                $image = Yii::$app->image->load($original);
+            }
+            catch (\Exception $e) {
+                return false;
+            }
 
-			foreach ($actions as $method => $args) {
-				call_user_func_array(array($image, $method), is_array($args) ? $args : array($args));
-			}
-			$image->save($this->getPath($version));
-		}
-		return true;
-	}
 
-	public function deleteFile()
-	{
-		$filePath = $this->getPath();
-		if (!$filePath) return;
+            foreach ($actions as $method => $args) {
+                call_user_func_array(array($image, $method), is_array($args) ? $args : array($args));
+            }
+            $image->save($this->getPath($version));
+        }
+        return true;
+    }
 
-		if (@file_exists($filePath)) {
-			@unlink($filePath);
-		}
+    public function deleteFile()
+    {
+        $filePath = $this->getPath();
+        if (!$filePath) return;
 
-		$this->deleteVersionImages();
-	}
+        if (@file_exists($filePath)) {
+            @unlink($filePath);
+        }
 
-	public function deleteVersionImages()
-	{
-		//create image preview for gallery manager
-		foreach ($this->versions as $version => $actions) {
-			if ($version == 'original') continue;
+        $this->deleteVersionImages();
+    }
 
-			$filePath = $this->getPath($version);
+    public function deleteVersionImages()
+    {
+        //create image preview for gallery manager
+        foreach ($this->versions as $version => $actions) {
+            if ($version == 'original') continue;
 
-			if (@file_exists($filePath)) {
-				@unlink($filePath);
-			}
-		}
-	}
+            $filePath = $this->getPath($version);
 
-	protected function afterUpload()
-	{
-		$this->updateImages();
-		parent::afterUpload();
-	}
+            if (@file_exists($filePath)) {
+                @unlink($filePath);
+            }
+        }
+    }
+
+    protected function afterUpload()
+    {
+        $this->updateImages();
+        parent::afterUpload();
+    }
 }
